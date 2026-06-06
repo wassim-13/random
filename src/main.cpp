10f52608db204d67eb1945ec196d2b44069ac9f8
wassim-13 <welboutaybi@gmail.com>
2026-06-06 17:51:52 +0100

    original main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-/*
 #include "run.h"
 #include "treat_args.h"
 
@@ -18,23 +17,4 @@ int main(int argc, char* argv[])
     run(app, args);
 
     return 0;
-}
-*/
-
-#include "stack.h"
-
-int main()
-{
-    Stack<int>* stack = new Stack<int>;
-    stack->push(5);
-    stack->push(3);
-    stack->push(9);
-    stack->push(7);
-
-    stack->print();
-
-    std::cout << "popint element\n";
-
-    stack->pop();
-    stack->print();
 }
